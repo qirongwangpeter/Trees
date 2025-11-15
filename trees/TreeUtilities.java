7fe51db1c8ad316a3f5159ec61553db792ab5d5b
Qirong Wang <qirongwang@umass.edu>
2025-11-15 18:23:18 -0500

    tried adding parents to intoBalanced, but there is a trick!

--- trees/TreeUtilities.java
+++ trees/TreeUtilities.java
@@ -55,7 +55,9 @@ public class TreeUtilities<E extends Comparable<E>> implements TU<E>{
         int mid = left + (right - left)/2;
         Node<E> n = new Node<>(list.get(mid));//each recursion will start a new root.
         n.left = intoBalancedHelper(list, left, mid - 1);
+        n.left.parent = n;
         n.right = intoBalancedHelper(list, mid + 1, right);
+        n.right.parent = n;
         return n;
     }
 //-----------------------------------------------------------------------------
