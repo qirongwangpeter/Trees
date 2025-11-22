0b575357f404a6cedc78fe8cead974f9c4e05abe
Qirong Wang <qirongwang@umass.edu>
2025-11-22 18:26:56 -0500

    added more methods, still not completed yet

--- trees/AVLTree.java
+++ trees/AVLTree.java
@@ -1,5 +1,6 @@
 package trees;
 import support.trees.*;
+import trees.TreeUtilities;
 
 public class AVLTree<E extends Comparable<E>> implements AVLT<E>{
     Node<E> root;
@@ -15,7 +16,7 @@ public class AVLTree<E extends Comparable<E>> implements AVLT<E>{
         }
         return 1+Math.max(height(node.left),height(node.right));
     }
-
+//----------------------------------------------------------------------------------------------------------------
     public Node<E> find(Node<E> node, E e){
         if(node == null){// the first two said that only two cases:contain or not.
             return null;
@@ -33,7 +34,8 @@ public class AVLTree<E extends Comparable<E>> implements AVLT<E>{
     public E find(E e){
         return find(root, e).data;
     }
-
+//----------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
     public void add(E e){
         if(root == null){
             root = new Node<>(e);
@@ -64,6 +66,50 @@ public class AVLTree<E extends Comparable<E>> implements AVLT<E>{
                 }
             }
         }
+    }
+//-----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+    public boolean isBST(Node<E> n){
+        if(n == null){//not necessary, but quick to end! but for most of the time we need to run this if, which is a waste of time...
+            return true;
+        }
+        return isBSTHelper(n,null,null);
+    }
+    private boolean isBSTHelper(Node<E> n, E min, E max){
+        if(n == null){
+            return true;
+        }else{
+            if(min != null && min.compareTo(n.data) >= 0){
+                return false;
+            }
+            if(max != null && max.compareTo(n.data) <= 0){
+                return false;
+            }
+            return isBSTHelper(n.left,min,n.data) && isBSTHelper(n.right,n.data,max);
+        }
+    }
+//----------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
+    public boolean isAVL(Node<E> n){
+        if(!isBST(n)){
+            return false;
+        }
+        return isAVLHelper(n);
+    }
+    private boolean isAVLHelper(Node<E> n){
+        //I am gonna to write the least efficient but reliable code for this! 
+    }
+//-----------------------------------------------------------------------------------------------------------------
+
+    public void insertioncheck(Node<E> node){
+        String path = "";
+        Node<E> n = node;//we will check each node from bottom all the way to the root one by one.
+        while(true){
+            if(!isAVL()){
+
+            }
+        }
+
     }
     private void rotateleft(Node<E> node){
         if(node == null || node.right == null){
@@ -98,7 +144,34 @@ public class AVLTree<E extends Comparable<E>> implements AVLT<E>{
     }
 
     private void rotateright(Node<E> node){
+        if(node == null || node.left == null){
+            return;
+        }
+        Node<E> A, B, T1, T2, T3;
+        A = node;
+        B = A.left;
+        T1 = A.right;
+        T3 = B.left;
+        T2 = B.right;
+        if(A == root){
+            root = B;
+            B.parent = null;
+        }else{
+            if(A.parent.left == A){
+                A.parent.left = B;
+                B.parent = A.parent;
+            }else{
+                A.parent.right = B;
+                B.parent = A.parent;
+            }
+        }
+        B.right = A;
+        A.parent = B;
 
+        A.left = T2;
+        if(T2 != null){
+            T2.parent = A;
+        }
     }
 
 }
