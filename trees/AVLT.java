dff4a29e4798d94fa4eef9b75b143aefad875297
Qirong Wang <qirongwang@umass.edu>
2025-11-22 19:30:33 -0500

    implemented more methods, but still not completed yet.

--- trees/AVLT.java
+++ trees/AVLT.java
@@ -3,11 +3,10 @@ import support.trees.*;
 public interface AVLT<E extends Comparable<E>> {
     int size();
     int height(Node<E> n);
-    boolean isAVL();
-    boolean contains();
-    Node<E> find(Node<E> node, E e);
-    E get();
-    void add();
-    E remove();
-    
+    boolean isAVL(Node<E> n);
+    boolean contains(E e);
+    Node<E> find(E e);
+    E get(E e);
+    void add(E e);
+    E remove(E e);
 }
